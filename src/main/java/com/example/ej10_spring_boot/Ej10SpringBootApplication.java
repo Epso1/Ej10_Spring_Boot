9d499e1699de934fe97263bd76db3d4470cc8f1c
Epso1 <cesaralvarorubert@gmail.com>
2023-12-18 16:44:43 +0100

    Initial commit

---
+++ src/main/java/com/example/ej10_spring_boot/Ej10SpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.ej10_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ej10SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ej10SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ej10_spring_boot/Ej10SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ej10_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ej10SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
